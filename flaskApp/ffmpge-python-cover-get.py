9241ee5cc29215eb92b66ee45b30db19dc6173b6
GL <1404919041@qq.com>
2020-05-03 12:01:34 +0800

    basic ffmepeg gen cover script success

--- flaskApp/ffmpge-python-cover-get.py
+++ flaskApp/ffmpge-python-cover-get.py
@@ -9,18 +9,43 @@ upPath = Path("./static/upside/")
 
 picOutBase = Path("./static/covers")
 
-for (dirpath, dirnames, filenames) in os.walk(upPath):
-    if item.suffix == ".mp4":
-        subprocess.call(
-            [
-                "ffmpeg",
-                "-y",
-                "-i",
-                item,
-                "-ss",
-                "00:00:05.000",
-                "-vframes",
-                "1",
-                picOutBase / item.name,
-            ]
-        )
+
+def gen_video_cover(rootPath):
+    """gen_video_cover
+    """
+    for item in rootPath.iterdir():
+        if item.is_dir():
+            gen_video_cover(item)
+        elif item.is_file() and item.suffix == ".mp4":
+            print(item.name)
+            subprocess.call(
+                [
+                    "ffmpeg",
+                    "-y",
+                    "-i",
+                    item,
+                    "-ss",
+                    "00:01:00.000",
+                    "-vframes",
+                    "1",
+                    picOutBase / (str(item.stem) + ".jpg"),
+                ]
+            )
+
+
+gen_video_cover(upPath)
+# for (dirpath, dirnames, filenames) in os.walk(upPath):
+#     if item.suffix == ".mp4":
+#         subprocess.call(
+#             [
+#                 "ffmpeg",
+#                 "-y",
+#                 "-i",
+#                 item,
+#                 "-ss",
+#                 "00:00:05.000",
+#                 "-vframes",
+#                 "1",
+#                 picOutBase / item.name,
+#             ]
+#         )
