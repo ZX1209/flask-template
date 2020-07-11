97ebf67cc9a02712f2b858a05e30216382a6af69
GL <1404919041@qq.com>
2020-07-11 21:17:35 +0800

    try get ip in start

--- startflaskApp.py
+++ startflaskApp.py
@@ -2,7 +2,13 @@ from flaskApp import app
 from get_local_addr import getLANip
 
 print("-" * 10)
-print(f"view via http://{getLANip()}:5555/")
+try:
+    print(f"view via http://{getLANip()}:5555/")
+
+except:
+    print("no net")
+    pass
+
 print("-" * 10)
 
 app.run(host="0.0.0.0", port=5555)
