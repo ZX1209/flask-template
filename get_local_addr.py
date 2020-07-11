d2ad3c2ca6b9220397cc813c9143987b33621c4c
GL <1404919041@qq.com>
2020-07-11 21:16:20 +0800

    update ip address for on internet env

--- get_local_addr.py
+++ get_local_addr.py
@@ -6,11 +6,22 @@ def getLANip():
     查询本机ip地址
     :return: ip
     """
+    ipstr = "no net?"
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(("8.8.8.8", 80))
-        ip = s.getsockname()[0]
+        ipstr = s.getsockname()[0]
+    except:
+        import subprocess
+
+        cmdstr = 'ifconfig | grep -E "inet[^6]"'
+        proc = subprocess.Popen(
+            cmdstr, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+
+        results = proc.communicate()
+        ipstr = str(results[0], encoding="utf-8")
     finally:
         s.close()
 
-    return ip
+    return ipstr
